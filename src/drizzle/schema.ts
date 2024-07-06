4f7fa89dfd5bb43b9c6b04d5050e31822fe26cfe
Moritz Schüßler <mail@moritzschuessler.de>
2024-07-06 04:41:00 +0200

    feat: add Drizzle-ORM
    This commit adds Drizzle-ORM for Auth and App-Data. Drizzle is configured for Postgres and Vercel. (might change later)

---
+++ src/drizzle/schema.ts
@@ -0,0 +1,9 @@
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uniqueIndex,
+} from "drizzle-orm/pg-core";
